fffd0fe2d63fc5a213195756663db983d35066c6
Hugo <hugo@boahstudio.com>
2024-03-31 00:33:22 -0300

    Login y New Account

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,8 +3,12 @@ export default function ShopLayout({ children }: {
  children: React.ReactNode;
 }) {
   return (
-    <main className="bg-gray-500 min-h-screen">
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">
+
         { children }
+        
+      </div>
     </main>
   );
 }
