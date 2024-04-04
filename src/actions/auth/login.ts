63a70b15069f62de166b2eff8ecad488f6b53a71
Hugo <hugo@boahstudio.com>
2024-04-04 12:58:53 -0300

    Login Action

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,31 @@
+'use server'
+
+
+import { AuthError } from 'next-auth'
+import { signIn } from '@/auth.config'
+import { sleep } from '@/utils';
+ 
+// ...
+ 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await sleep(2)
+
+    console.log('formData', formData);
+    
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
