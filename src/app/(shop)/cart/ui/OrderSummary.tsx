545ac390e97e30b861e4f172a4d2a2387c33b714
Hugo <hugo@boahstudio.com>
2024-04-04 01:51:44 -0300

    bugfix in OrderSummary

--- src/app/(shop)/cart/ui/OrderSummary.tsx
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -1,18 +1,29 @@
 'use client'
 
 import { useCartStore } from "@/store"
+import { useEffect, useState } from "react"
 
 export const OrderSummary = () => {
   
   const getSummaryInformation = useCartStore( state => state.getSummaryInformation() )
   const { totalItems, subTotalPrice, tax, totalPrice } = getSummaryInformation
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+  
+
+  if( !loaded ) {
+    return <p>Cargando...</p>
+  }
   
   return (
     <>
       <div className="grid grid-cols-2">
 
         <span>No. Productos</span>
-        <span className="text-right">{ totalItems } artículos</span>
+        <span className="text-right">{ totalItems === 1 ? '1 artículo' : `${totalItems} artículos` } </span>
 
         <span>Subtotal</span>
         <span className="text-right">$ { subTotalPrice }</span>
